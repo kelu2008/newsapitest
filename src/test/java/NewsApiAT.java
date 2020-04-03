07cdefe46bb0abea85277c142849296597eb1bba
KE LU <kelu2008@gmail.com>
2020-04-03 22:10:14 +0100

    updated for Q1

--- src/test/java/NewsApiAT.java
+++ src/test/java/NewsApiAT.java
@@ -13,16 +13,54 @@ import static org.junit.Assert.assertTrue;
 public class NewsApiAT extends TestBase {
 
 
-    // return true if fresh stories are available in dublin city area since yesterday at 00:00:00.0000AM
+    // Compare the volumes of fresh stories in 5 minutes today and the volumes of fresh stories in the same period yesterday
+    // This is not a real junit test, i just use it to implement the code below to return the figures
     @Test
-    public void testFreshStoriesToday() {
+    public void compareFreshStoriesBetweenTodayAndYesterday() {
+
+        Response getFreshStoriesResultToday = given().spec(requestSpecification).
+                basePath("/stories").
+                header("X-AYLIEN-NewsAPI-Application-ID", NEWS_API_APPLICATION_ID).
+                header("X-AYLIEN-NewsAPI-Application-Key", NEWS_API_APPLICATION_KEY).
+                //queryParam("source.locations.city[]", "dublin").
+                        queryParam("published_at.start", "NOW-5MINUTES").
+                        queryParam("published_at.end", "NOW").
+                        when().get();
+
+        assertEquals(HttpStatus.SC_OK, getFreshStoriesResultToday.statusCode());
+
+        JsonPath jsonPathEvaluatorToday = getFreshStoriesResultToday.jsonPath();
+        int volumesOfFreshStoriesToday = jsonPathEvaluatorToday.getList("stories").size();
+
+        Response getFreshStoriesResultYesterday = given().spec(requestSpecification).
+                basePath("/stories").
+                header("X-AYLIEN-NewsAPI-Application-ID", NEWS_API_APPLICATION_ID).
+                header("X-AYLIEN-NewsAPI-Application-Key", NEWS_API_APPLICATION_KEY).
+                //queryParam("source.locations.city[]", "dublin").
+                        queryParam("published_at.start", "NOW-5MINUTES-1DAY").
+                        queryParam("published_at.end", "NOW-1DAY").
+                        when().get();
+
+        assertEquals(HttpStatus.SC_OK, getFreshStoriesResultYesterday.statusCode());
+
+        JsonPath jsonPathEvaluatorYesterday = getFreshStoriesResultYesterday.jsonPath();
+        int volumesOfFreshStoriesYesterday = jsonPathEvaluatorYesterday.getList("stories").size();
+
+        System.out.println("The volumes of fresh stories in last 5 minutes today " + volumesOfFreshStoriesToday);
+        System.out.println("The volumes of fresh stories in last 5 minutes yesterday " + volumesOfFreshStoriesYesterday);
+    }
+
+
+    // return true if fresh stories are available in last 30 minutes
+    // It is not related to QA challenge, just checking to ensure the fresh stories should be available in last 30 minutes
+    @Test
+    public void testFreshStoriesInLast30MinutesToday() {
 
         Response getStoriesResult = given().spec(requestSpecification).
                 basePath("/stories").
                 header("X-AYLIEN-NewsAPI-Application-ID", NEWS_API_APPLICATION_ID).
                 header("X-AYLIEN-NewsAPI-Application-Key", NEWS_API_APPLICATION_KEY).
-                queryParam("source.locations.city[]", "dublin").
-                queryParam("published_at.start", "NOW-1DAY/DAY").
+                queryParam("published_at.start", "NOW-6HOURS").
                 queryParam("published_at.end", "NOW").
                 when().get();
 
